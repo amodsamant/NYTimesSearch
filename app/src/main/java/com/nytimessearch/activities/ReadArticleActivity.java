b7d120c505e1dcbc7a0eaad74ca3c3fbae3e57d3
amodsamant <samant.amod@gmail.com>
2017-03-18 21:37:05 -0700

    Added chrome custom tabs

--- app/src/main/java/com/nytimessearch/activities/ReadArticleActivity.java
+++ app/src/main/java/com/nytimessearch/activities/ReadArticleActivity.java
@@ -1,11 +1,21 @@
 package com.nytimessearch.activities;
 
 import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +24,7 @@ import com.nytimessearch.R;
 import com.nytimessearch.models.NYTArticle;
 
 import org.parceler.Parcels;
+
 
 public class ReadArticleActivity extends AppCompatActivity {
 
@@ -28,17 +39,57 @@ public class ReadArticleActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         article = (NYTArticle) Parcels.unwrap(getIntent().getParcelableExtra("article"));
-        webView = (WebView) findViewById(R.id.web_view);
 
-        webView.getSettings().setLoadsImagesAutomatically(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        // Configure the client to use when opening URLs
-        webView.setWebViewClient(new MyBrowser());
-        // Load the initial URL
-        webView.loadUrl(article.getWebUrl());
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(this,R.color.colorAccent));
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_action_share);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, article.getWebUrl());
+
+        int requestCode = 100;
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                requestCode,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setActionButton(bitmap, "Share Link", pendingIntent, true);
+
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(article.getWebUrl()));
+
+//        webView = (WebView) findViewById(R.id.web_view);
+//
+//        webView.getSettings().setLoadsImagesAutomatically(true);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+//        // Configure the client to use when opening URLs
+//        webView.setWebViewClient(new MyBrowser());
+//        // Load the initial URL
+//        webView.loadUrl(article.getWebUrl());
 
         getSupportActionBar().setTitle(article.getWebUrl());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.menu_read_article, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider provider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, article.getWebUrl());
+
+        provider.setShareIntent(shareIntent);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     // Manages the behavior when URLs are loaded
