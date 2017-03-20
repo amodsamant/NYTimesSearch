e0cf8b7e1f9ebaf0096c4b2ad9953bb06bcf01cb
amodsamant <samant.amod@gmail.com>
2017-03-19 18:13:05 -0700

    Added splash screen

---
+++ app/src/main/java/com/nytimessearch/activities/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.nytimessearch.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, SearchActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
+
+    }
+}
